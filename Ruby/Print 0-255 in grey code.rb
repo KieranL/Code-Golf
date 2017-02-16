e48586611fbb77a25aa0d8f2e91265c3183d72f3
Kieran Loewen <kieranloewen@live.com>
2017-02-16 08:11:45 -0600

    Update Print 0-255 in grey code.rb

--- Ruby/Print 0-255 in grey code.rb
+++ Ruby/Print 0-255 in grey code.rb
@@ -1,3 +1,5 @@
 #http://codegolf.stackexchange.com/questions/110204/print-0-255-in-grey-code/110209#110209
 
-256.times{|i|p (i^(i>>1)).to_s(2).rjust 8,?0}
+#256.times{|i|puts (i^(i>>1)).to_s(2).rjust 8,?0} #48 Bytes
+
+256.times{|i|puts "%08b"%(i^i/2)} #33 Bytes
